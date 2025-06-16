e17b194e2e610e0bc10e851211433ac760a37521
Francisco Ignácio Fontoura Solla <francisco.solla@gmail.com>
2025-06-15 23:32:49 -0300

    landing

---
+++ apps/landing/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="h-dvh bg-blue-950">
+      <h1 className="text-white">
+        Seu mundo digital.
+        <br />
+        <span>Suas regras.</span>
+      </h1>
+    </main>
+  );
+}
