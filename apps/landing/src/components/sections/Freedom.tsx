0c054354d5fa2fcc22066cd831e7f7bdc0252ddf
Francisco Ignácio Fontoura Solla <francisco.solla@gmail.com>
2025-06-25 23:02:31 -0300

    update

--- apps/landing/src/components/sections/Freedom.tsx
+++ apps/landing/src/components/sections/Freedom.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import { NoWrap } from "../atoms/NoWrap";
 
 export const Freedom = () => (
-  <section className="snap-start h-dvh w-dvw relative flex overflow-hidden p-5 md:pt-[6dvh] lg:pt-[9dvh] md:px-17.5">
+  <section className="snap-start h-dvh w-dvw relative flex overflow-hidden p-5 md:pt-[6dvh] lg:pt-[9dvh] md:px-17.5 bg-linear-0 to-[#DCDCDC] from-[#C1C1C1]">
     <div className="z-10 flex flex-col layout-max-width w-full h-full flex-1 text-black">
       <div className="flex flex-col gap-3">
         <h2>MOVE</h2>
-        <h3 className="opacity-30">WITHOUT PERMISSION</h3>
-        <h3 className="opacity-20">WITH CONFIDENCE</h3>
-        <h3 className="opacity-10">WITH INTENTION</h3>
+        <h3 className="opacity-35">WITHOUT PERMISSION</h3>
+        <h3 className="opacity-25">WITH CONFIDENCE</h3>
+        <h3 className="opacity-15">WITH INTENTION</h3>
       </div>
       <div className="flex-1" />
       <p className="text-right">
@@ -27,7 +27,7 @@ export const Freedom = () => (
       width={3840}
       height={6922}
       alt="Silhouette of two people joyfully leaning out of a car on an empty road at sunset, arms raised in celebration, capturing a feeling of freedom and adventure."
-      className="absolute h-full w-auto min-w-full md:h-auto md:w-full left-0 top-0 md:-translate-y-[10%] landscape:lg:-translate-y-[41%] landscape:xl:-translate-y-[44%] landscape:2xl:-translate-y-[46%]"
+      className="absolute w-full left-0 bottom-0 landscape:translate-y-[65%]"
     />
   </section>
 );
