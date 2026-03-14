288ada421f90c531e69d2cce92cdc515bc153697
Francisco Solla <fsolla@pm.me>
2026-03-14 18:50:44 -0300

    prepare project to deploy on server host

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_BASE_PATH || ''
+const assetPrefix = process.env.NEXT_ASSET_PREFIX || basePath || undefined
+const allowedImageHost = process.env.NEXT_PUBLIC_SITE_URL
+
 const nextConfig = {
+  basePath,
+  assetPrefix,
   images: {
-    remotePatterns: [new URL('https://jorgesolla.com.br/**')],
+    remotePatterns: [new URL(`${allowedImageHost}/**`)],
   },
 }
 
