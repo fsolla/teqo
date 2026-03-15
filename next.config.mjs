f83d914133e81caea1d870a050f12190f268439c
Francisco Solla <fsolla@pm.me>
2026-03-15 13:46:44 -0300

    prepare for deploy through cPanel

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_BASE_PATH || ''
-const assetPrefix = process.env.NEXT_ASSET_PREFIX || basePath || undefined
-const allowedImageHost = process.env.NEXT_PUBLIC_SITE_URL
+const assetPrefix = process.env.NEXT_ASSET_PREFIX || undefined
+const allowedImageHost = process.env.NEXT_PUBLIC_SITE_URL || 'https://jorgesolla.com.br'
 
 const nextConfig = {
   basePath,
--- src/app/(frontend)/mandato-no-whatsapp/page.tsx
+++ src/app/(frontend)/mandato-no-whatsapp/page.tsx
@@ -3,8 +3,13 @@ import { WhatsappForm } from '@/components/WhatsappForm'
 import Image from 'next/image'
 
 export default function Page() {
+  const basePath = process.env.NEXT_BASE_PATH || ''
+
   return (
-    <main className="w-screen h-screen bg-[url(/BG.jpg)] bg-center bg-cover flex flex-col items-center justify-center">
+    <main
+      className="w-screen h-screen bg-center bg-cover flex flex-col items-center justify-center"
+      style={{ backgroundImage: `url(${basePath}/BG.jpg)` }}
+    >
       <div className="flex flex-col max-w-300 gap-12">
         <div className="relative flex flex-col gap-5">
           <h1 className="text-5xl text-left">
@@ -26,14 +31,20 @@ export default function Page() {
           <WhatsappForm classname="flex-1 w-[40%]" />
           <Image
             className="flex-[1.8] -mx-10 absolute top-1/2 -translate-y-[50%] right-0 w-[67%]"
-            src="/LULA_E_SOLLA.png"
+            src={`${basePath}/LULA_E_SOLLA.png`}
             width={956}
             height={948}
             alt=""
           />
         </div>
         <div className="relative flex flex-col items-center gap-0">
-          <Image src="LOGO_SOLLA_BRANCO.svg" width={430} height={300} alt="" className="-my-30" />
+          <Image
+            src={`${basePath}/LOGO_SOLLA_BRANCO.svg`}
+            width={430}
+            height={300}
+            alt=""
+            className="-my-30"
+          />
           <p className="text-center">
             <strong>Seus dados estão seguros.</strong> Utilizamos suas informações apenas para
             contato e envio de conteúdos relacionados.
