a2ad8f7e20799335fc908a6523d5429740dd6140
Francisco Solla <fsolla@pm.me>
2026-03-15 15:28:48 -0300

    revert changes made to run on cPanel

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_BASE_PATH || ''
-const assetPrefix = process.env.NEXT_ASSET_PREFIX || undefined
 const allowedImageHost = process.env.NEXT_PUBLIC_SITE_URL || 'https://jorgesolla.com.br'
 
 const nextConfig = {
-  basePath,
-  assetPrefix,
   images: {
     remotePatterns: [new URL(`${allowedImageHost}/**`)],
   },
